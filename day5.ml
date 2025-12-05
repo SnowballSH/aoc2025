6da1b098792405693bef953b7fd356f3f114d7ad
SnowballSH <derek070211@gmail.com>
2025-12-05 00:23:49 -0500

    day5

---
+++ day5.ml
@@ -0,0 +1,75 @@
+open Printf
+open Scanf
+open String
+
+let read_lines (file_name : string) : string list =
+  In_channel.with_open_text file_name In_channel.input_lines
+
+(* AoC 2025 Day 5 *)
+
+(* Helpers *)
+let read_range s = sscanf s "%Ld-%Ld" (fun a b -> (a, b))
+let read_int s = sscanf s "%Ld" (fun x -> x)
+let ( ++ ) a b = Int64.add a b
+let ( -- ) a b = Int64.sub a b
+
+(* Part 1 *)
+let solve_all_part_1 all_lines =
+  let rec ranges rem acc =
+    match rem with
+    | [] -> (rem, acc)
+    | "" :: t -> (t, acc)
+    | h :: t -> ranges t (read_range h :: acc)
+  in
+  let remaining_lines, rng = ranges all_lines [] in
+  let rec count_occ x rem =
+    match rem with
+    | [] -> false
+    | h :: t ->
+        let a, b = h in
+        if a <= x && x <= b then true else count_occ x t
+  in
+  let rec solve rem acc =
+    match rem with
+    | [] -> acc
+    | h :: t ->
+        let ok = count_occ (read_int h) rng in
+        solve t (acc + Bool.to_int ok)
+  in
+  solve remaining_lines 0
+
+(* Part 2 *)
+let solve_all_part_2 all_lines =
+  let rec solve cur rem acc =
+    let l1, r1 = cur in
+    match rem with
+    | [] -> acc ++ (r1 -- l1 ++ 1L)
+    | nxt :: t ->
+        let l2, r2 = nxt in
+        if l2 > r1 then solve nxt t (acc ++ (r1 -- l1 ++ 1L))
+        else if r2 > r1 then solve nxt t (acc ++ (l2 -- l1))
+        else solve cur t acc
+  in
+  let rec ranges rem acc =
+    match rem with
+    | [] -> acc
+    | "" :: t -> acc
+    | h :: t -> ranges t (read_range h :: acc)
+  in
+  let rng = ranges all_lines [] in
+  let a =
+    List.sort
+      (fun (a1, b1) (a2, b2) ->
+        match Int64.compare a1 a2 with 0 -> Int64.compare b1 b2 | c -> c)
+      rng
+  in
+  match a with [] -> 0L | h :: t -> solve h t 0L
+
+(* Driver *)
+let () =
+  let lines = read_lines "day5.in" in
+  let ans1 = solve_all_part_1 lines in
+  printf "Part 1 = %d\n" ans1;
+  let ans2 = solve_all_part_2 lines in
+  printf "Part 2 = %Ld\n" ans2;
+  ()
