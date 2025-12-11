82dc4c134fc9192cc2147f15bf0ea9839ba38c3d
SnowballSH <derek070211@gmail.com>
2025-12-10 21:14:16 -0500

    cleanup day10

--- day10.py
+++ day10.py
@@ -1,43 +1,29 @@
-from itertools import product
 from z3 import Optimize, Int, sat
 
 
 def parse(line):
     blocks = line.split(" ")
-    config, *buttons, req = blocks
+    config, *buttons, joltage = blocks
     config = config[1:-1]
     buttons = [[int(x) for x in b[1:-1].split(",")] for b in buttons]
-    req = [int(x) for x in req.strip()[1:-1].split(",")]
-    return buttons, config, req
+    joltage = [int(x) for x in joltage.strip()[1:-1].split(",")]
+    return buttons, config, joltage
 
 
 def solve(line, part):
-    buttons, config, goal = parse(line)
-
-    if part == 1:
-        goal = [int(x == "#") for x in config]
-
+    buttons, config, joltage = parse(line)
+    goal = [x == "#" for x in config] if part == 1 else joltage
     bs = [set(x) for x in buttons]
     s = Optimize()
-    variables = [Int(f"b{i}") for i in range(len(buttons))]
-    for x in variables:
-        s.add(x >= 0)
-        if part == 1:
-            s.add(x <= 1)
+    variables = [Int(str(i)) for i in range(len(buttons))]
+    s.add(x >= 0 for x in variables)
     for i, g in enumerate(goal):
-        acc = 0
-        for j, b in enumerate(bs):
-            if i in b:
-                acc = acc + variables[j]
-        if part == 1:
-            s.add(acc % 2 == g)
-        else:
-            s.add(acc == g)
+        acc = sum(variables[j] for j, b in enumerate(bs) if i in b)
+        s.add(acc % 2 == g if part == 1 else acc == g)
     s.minimize(sum(variables))
     assert s.check() == sat
-    m = s.model()
-    ans = sum(m[x].as_long() for x in variables)
-    return ans
+    soln = s.model()
+    return sum(soln[x].as_long() for x in variables)
 
 
 with open("day10.in") as f:
